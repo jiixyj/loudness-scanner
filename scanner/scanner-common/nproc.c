f324517c9d80f2843eb25e763b1287b6821828ad
Jan Kokemüller <jan.kokemueller@gmail.com>
2021-12-24 08:24:59 +0100

    use 'g_get_num_processors' to get number of processors

--- scanner/scanner-common/nproc.c
+++ scanner/scanner-common/nproc.c
@@ -4,24 +4,8 @@
 
 #include <glib.h>
 
-#if defined(G_OS_UNIX)
-#include <unistd.h>
-#elif defined(G_OS_WIN32)
-#include <windows.h>
-#endif
-
 int
 nproc(void)
 {
-	int ret = 1;
-#if defined(G_OS_UNIX) && defined(_SC_NPROCESSORS_ONLN)
-	ret = (int)sysconf(_SC_NPROCESSORS_ONLN);
-	if (ret < 0)
-		ret = 1;
-#elif defined(G_OS_WIN32)
-	SYSTEM_INFO sysinfo;
-	GetSystemInfo(&sysinfo);
-	ret = sysinfo.dwNumberOfProcessors;
-#endif
-	return ret;
+	return (int)g_get_num_processors();
 }
